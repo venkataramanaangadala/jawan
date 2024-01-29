35d3429c1d8729e6c6b1e2a6cdfbb9ea7dcb19ee
venkataramanaangadala <155218269+venkataramanaangadala@users.noreply.github.com>
2024-01-29 10:03:00 +0530

    Create jawan.groovy

---
+++ vars/jawan.groovy
@@ -0,0 +1,14 @@
+def sum of (a,b)
+{
+  echo"The sum of ${a} and ${b} is ${a+b} 
+}
+
+def sub (a,b)
+{
+  echo"The sub of ${a} and ${b} is ${a-b}
+}  
+
+def mul (a,b)
+{
+  echo"The mul of ${a} and ${b} is ${a*b}
+}  
